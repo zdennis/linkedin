f9da1296075473ecfd0fb33ee91760d8f7424d7f
Eric Himmelreich <eahimmel@gmail.com>
2010-06-09 00:12:53 +0800

    added example how to fetch auth user's public profile url

--- examples/profile.rb
+++ examples/profile.rb
@@ -5,6 +5,10 @@
 # get the profile for the authenticated user
 client.profile
 
+# get the public profile url for the authenticated user
+client.profile(:fields => 'public-profile-url').
+  relation_to_viewer['public-profile-url']
+
 # get a profile for someone found in network via ID
 client.profile(:id => 'gNma67_AdI')
 
